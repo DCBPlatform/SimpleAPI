2d8dd0eb97449aba9061cb9bca4768a051fc2662
Afeez Aziz <afeezaziz@gmail.com>
2021-01-24 23:25:27 +0800

    a

--- api/blockchain.py
+++ api/blockchain.py
@@ -1,12 +1,17 @@
 from substrateinterface.contracts import ContractCode, ContractInstance
 from substrateinterface import SubstrateInterface, Keypair
 
-import ssl
+import ssl 
 
-ssl._create_default_https_context = ssl._create_unverified_context
+try:
+    _create_unverified_https_context = ssl._create_unverified_context
+except AttributeError:
+    pass
+else:
+    ssl._create_default_https_context = _create_unverified_https_context
 
 substrate = SubstrateInterface(
-    url="https://1.dcb.my",
+    url="wss://1.dcb.my",
     ss58_format=42,
     type_registry_preset='substrate-node-template'
 )
