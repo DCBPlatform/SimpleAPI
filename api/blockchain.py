dfe73cf1a2260580bc39bc5f8e4696b52a1cb650
Afeez Aziz <afeezaziz@gmail.com>
2021-01-28 14:15:32 +0800

    dsa

--- api/blockchain.py
+++ api/blockchain.py
@@ -8,8 +8,17 @@ from websocket import create_connection, WebSocketConnectionClosedException
 
 
 
+def connect():
+    ws = create_connection(config('NODE_URL'),
+        max_size=2 ** 32,
+        read_limit=2 ** 32,
+        write_limit=2 ** 32,
+        sslopt={"cert_reqs": ssl.CERT_NONE})
+
+    return ws
+
 substrate = SubstrateInterface(
-    websocket=config('NODE_URL'),
+    websocket=connect(),
     ss58_format=42,
     type_registry_preset='substrate-node-template'
 )
